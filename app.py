8f40e75a2ef1f2127456aed2f3d8c46a5ef1031f
Jonathan Alejandro Castro Inca <1160550@alumno.um.edu.mx>
2022-02-10 17:21:46 -0600

    configuracion inicial y primera ruta

--- app.py
+++ app.py
@@ -1,64 +1,30 @@
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
-from products import products
+# find Place
 
-# Testing Route
-@app.route('/ping', methods=['GET'])
-def ping():
-    return jsonify({'response': 'pong!'})
-
-# Get Data Routes
-@app.route('/products')
-def getProducts():
-    # return jsonify(products)
-    return jsonify({'products': products})
-
-
-@app.route('/products/<string:product_name>')
-def getProduct(product_name):
-    productsFound = [
-        product for product in products if product['name'] == product_name.lower()]
-    if (len(productsFound) > 0):
-        return jsonify({'product': productsFound[0]})
-    return jsonify({'message': 'Product Not found'})
-
-# Create Data Routes
-@app.route('/products', methods=['POST'])
-def addProduct():
-    new_product = {
-        'name': request.json['name'],
-        'price': request.json['price'],
-        'quantity': 10
+@app.route('/find-place', methods=['POST'])
+def call_api_maps():
+    query = {
+        'fields': 'formatted_address,name,rating,opening_hours,geometry',
+        'input': request.json['text'],
+        'inputtype': 'textquery',
+        'key': 'AIzaSyC5Lz0BuGKtNqVmXhkqDw-xiuPGGRl3O7Q'
     }
-    products.append(new_product)
-    return jsonify({'products': products})
+    api_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+    try:
+        response = requests.get(api_url, params=query)
+        result = response.json()
+        if result['status'] == 'OK':
+            return result
+        else:
+            return jsonify({'status': 'ZERO_RESULTS', 'message': 'No results found'})
 
-# Update Data Route
-@app.route('/products/<string:product_name>', methods=['PUT'])
-def editProduct(product_name):
-    productsFound = [product for product in products if product['name'] == product_name]
-    if (len(productsFound) > 0):
-        productsFound[0]['name'] = request.json['name']
-        productsFound[0]['price'] = request.json['price']
-        productsFound[0]['quantity'] = request.json['quantity']
-        return jsonify({
-            'message': 'Product Updated',
-            'product': productsFound[0]
-        })
-    return jsonify({'message': 'Product Not found'})
+    except requests.exceptions.HTTPError as error:
+        print(error)
 
-# DELETE Data Route
-@app.route('/products/<string:product_name>', methods=['DELETE'])
-def deleteProduct(product_name):
-    productsFound = [product for product in products if product['name'] == product_name]
-    if len(productsFound) > 0:
-        products.remove(productsFound[0])
-        return jsonify({
-            'message': 'Product Deleted',
-            'products': products
-        })
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
